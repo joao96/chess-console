740bd06781ebed57f229c8d8e7554c8bafaa4b20
joao96 <jvpoletti@gmail.com>
2020-10-28 11:41:02 -0300

    ChessPosition class added

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -9,21 +9,7 @@ namespace ChessBoard
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Board board = new Board(8, 8);
-
-                board.InsertPiece(new Rook(board, Color.Black), new Position(0, 0));
-                board.InsertPiece(new Rook(board, Color.Black), new Position(1, 3));
-                board.InsertPiece(new King(board, Color.Black), new Position(0, 2));
-
-                Screen.PrintBoard(board);
-            }
-            catch (BoardException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
+            ChessPosition pos = new ChessPosition('a', 1);
         }
     }
 }
